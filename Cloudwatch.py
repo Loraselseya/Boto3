d30093ba04418f7b62470c4d9c80029a4abe3cdf
Loraselseya <124553825+Loraselseya@users.noreply.github.com>
2023-02-08 17:00:23 +0530

    initial commit

--- Cloudwatch.py
+++ Cloudwatch.py
@@ -1,34 +1,40 @@
-# import boto3
+# Create Cloudwatch Alarm
+import boto3
 
-# # Create CloudWatch client
-# cloudwatch = boto3.client('cloudwatch')
+# Create CloudWatch client
+cloudwatch = boto3.client('cloudwatch')
 
-# # Create alarm
-# cloudwatch.put_metric_alarm(
-#     AlarmName='Web_Server_CPU_Utilization',
-#     ComparisonOperator='GreaterThanThreshold',
-#     EvaluationPeriods=1,
-#     MetricName='CPUUtilization',
-#     Namespace='AWS/EC2',
-#     Period=60,
-#     Statistic='Average',
-#     Threshold=70.0,
-#     ActionsEnabled=False,
-#     AlarmDescription='Alarm when server CPU exceeds 70%',
-#     Dimensions=[
-#         {
-#           'Name': 'InstanceId',
-#           'Value': 'INSTANCE_ID'
-#         },
-#     ],
-#     Unit='Seconds'
-# )
+# Create alarm
+cloudwatch.put_metric_alarm(
+    AlarmName='Web_Server_CPU_Utilization',
+    ComparisonOperator='GreaterThanThreshold',
+    EvaluationPeriods=1,
+    MetricName='CPUUtilization',
+    Namespace='AWS/EC2',
+    Period=60,
+    Statistic='Average',
+    Threshold=70.0,
+    ActionsEnabled=False,
+    AlarmDescription='Alarm when server CPU exceeds 70%',
+    Dimensions=[
+        {
+          'Name': 'InstanceId',
+          'Value': 'INSTANCE_ID'
+        },
+    ],
+    Unit='Seconds'
+)
 
-# # List alarms of insufficient data through the pagination interface
-# paginator = cloudwatch.get_paginator('describe_alarms')
-# for response in paginator.paginate(StateValue='INSUFFICIENT_DATA'):
-#     print(response['MetricAlarms'])
 
+# List alarms of insufficient data through the pagination interface
+
+paginator = cloudwatch.get_paginator('describe_alarms')
+for response in paginator.paginate(StateValue='INSUFFICIENT_DATA'):
+    print(response['MetricAlarms'])
+
+
+
+# Terminate Cloudwatch
 import boto3
 
 # Create CloudWatch client
