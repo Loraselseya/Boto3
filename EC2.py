2023585a72850d51b216b848be962c10380672d0
Loraselseya <124553825+Loraselseya@users.noreply.github.com>
2023-02-08 16:45:17 +0530

    new

--- EC2.py
+++ EC2.py
@@ -9,3 +9,15 @@ ec2_resource.create_instances(
     MaxCount=1,
     MinCount=1
 )
+
+# Listing EC2
+
+import boto3
+
+AWS_REGION = "ap-south-1"
+EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
+
+instances = EC2_RESOURCE.instances.all()
+
+for instance in instances:
+    print("EC2 instance {instance.id}")
