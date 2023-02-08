330b66a6485beb4d977c12ac0711b3ddf1e5c8c3
Loraselseya <124553825+Loraselseya@users.noreply.github.com>
2023-02-08 17:04:52 +0530

    initial commit

---
+++ EBS.py
@@ -0,0 +1,25 @@
+import boto3
+
+AWS_REGION = "eu-west-2"
+
+ec2_client = boto3.client('ec2', region_name=AWS_REGION)
+
+new_volume = ec2_client.create_volume(
+    AvailabilityZone=f'{AWS_REGION}a',
+    Encrypted = True,
+    Size=10,
+    VolumeType='gp2',
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'EBS_volume_1'
+                }
+            ]
+        }
+    ]
+)
+
+print(f'Created volume ID: {new_volume["VolumeId"]}')
