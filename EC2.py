6744f0ef487f8a64120e493cea275fbe7b3735bf
Loraselseya <124553825+Loraselseya@users.noreply.github.com>
2023-02-08 15:58:21 +0530

    new

--- EC2.py
+++ EC2.py
@@ -1,4 +1,4 @@
-# Multiple EC2
+# Create EC2
 import boto3
 
 ec2_resource=boto3.resource("ec2")
@@ -6,6 +6,6 @@ ec2_resource=boto3.resource("ec2")
 ec2_resource.create_instances(
     ImageId='ami-01a4f99c4ac11b03c',
     InstanceType='t2.micro',
-    MaxCount=3,
-    MinCount=2
+    MaxCount=1,
+    MinCount=1
 )
