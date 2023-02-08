afa6f9219c7dba7fc9cbc81f54348a795462197c
Loraselseya <124553825+Loraselseya@users.noreply.github.com>
2023-02-08 14:38:55 +0530

    new

--- VPC.py
+++ VPC.py
@@ -2,4 +2,15 @@ import boto3
 
 client = boto3.client("ec2")
 
+# Create VPC
 client.create_vpc(CidrBlock='10.0.0.0/24')
+
+# Describe VPC
+import boto3
+x=client.describe_vpcs()
+#print(x)
+
+no_of_vpcs=x["Vpcs"]
+
+for vpc in no_of_vpcs:
+    print(vpc["VpcId"])
